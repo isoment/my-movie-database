bd00a1afc875cf152753af3ce86cb19c71a69536
twill <twill1@gmx.com>
2020-08-12 00:52:18 -0500

    Popular movies scroll on index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,8 @@
                                placeholder="Search for a movie, tv show, person...">
                         <input type="submit" value="Search" 
                                class="py-3 px-8 -ml-5 rounded-full bg-primary-red text-white 
-                                      text-sm font-bold focus:outline-none">
+                                      text-sm font-bold focus:outline-none hover:text-primary-blue-med cursor-pointer 
+                                      transition ease-in-out duration-300">
                     </form>
                 </div>
             </div>
@@ -24,7 +25,79 @@
 
         <section class="popular-media">
             <div class="popular-media-content mt-8 px-10">
-                <h2 class="text-2xl font-semibold font-mulish">Popular Now</h2>
+                <h2 class="text-2xl font-semibold font-mulish">Popular Movies</h2>
+                {{-- <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-8 mt-6"> --}}
+
+                <div class="flex overflow-x-scroll mt-6 pb-10">
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                 class="rounded-lg">
+                        </a>
+                        <h5>Title</h5>
+                        <span>Release Date</span>
+                    </div>
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>
+                    
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>  
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>  
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>  
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>  
+
+                    {{-- Card --}}
+                    <div class="index-movie-card w-40 mr-4">
+                        <a href="#">
+                            <img src="/img/Movie-Poster.jpg" alt="poster"
+                                    class="rounded-lg">
+                        </a>
+                    </div>  
+
+                
+                </div>
             </div>
         </section>
 
